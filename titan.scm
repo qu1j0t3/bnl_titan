311f999d517682bc68dc1ead621cf7c27e9f706e
Toby <toby@telegraphics.com.au>
2011-09-05 18:24:13 -0400

    Try to be comment studly before Quadrescence yells at me.

--- titan.scm
+++ titan.scm
@@ -1,19 +1,19 @@
-; This file is part of an assembler for bootnecklad's Titan
-; Copyright (C) 2011 Toby Thain, toby@telegraphics.com.au
-;
-; This program is free software; you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by  
-; the Free Software Foundation; either version 2 of the License, or
-; (at your option) any later version.
-;
-; This program is distributed in the hope that it will be useful,
-; but WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-; GNU General Public License for more details.
-;
-; You should have received a copy of the GNU General Public License  
-; along with this program; if not, write to the Free Software
-; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+;; This file is part of an assembler for bootnecklad's Titan
+;; Copyright (C) 2011 Toby Thain, toby@telegraphics.com.au
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by  
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License  
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 (use srfi-69) ; hash tables
 (include "titan.grm.scm")
@@ -25,25 +25,25 @@
                   ; pseudo-instructions
                   (MOV . #f)   (CLR . #f)))
 
-; ------ lexical analyser. produces stream of tokens for parser ------
+;;; ------ lexical analyser. produces stream of tokens for parser ------
 
 (define (make-lexer errorp)
 
-  ; is the character a letter or underscore?
+  ;; is the character a letter or underscore?
   (define (word-char? c)
     (or (char=? c #\_)
         (char-alphabetic? c)))
 
-  ; read a sequence of letters or underscores into a list
-  ; ends at a non-alphabetic, or EOF
-  ; the first character, already read, is provided as the parameter
+  ;; read a sequence of letters or underscores into a list
+  ;; ends at a non-word character, or EOF
+  ;; the first character, already read, is provided as the parameter
   (define (read-alpha c)
     (cons (char-upcase c)
       (if (word-char? (peek-char))
         (read-alpha (read-char))
         '())))
 
-  ; read a literal constant: a series of hex digits preceded by '0x'
+  ;; read a literal constant: a series of hex digits preceded by '0x'
   (define (read-const)
     (define (read-hex)
       (let loop ((acc 0)
@@ -60,7 +60,7 @@
           'bad-address))
       'expected-0x))
 
-  ; eat characters up to newline (or EOF)
+  ;; eat characters up to newline (or EOF)
   (define (skip-comment)    
     (let ((c (read-char)))  ; eat one
       (if (or (eof-object? c)
@@ -77,12 +77,12 @@
           (list 'bad-register (car lst))))
       (let* ((sym (string->symbol (list->string lst)))
              (lkp (assv sym opcodes)))
-        (if (pair? lkp)
+        (if lkp
           (car lkp)
           (list 'bad-opcode sym)))))
 
-  ; note that new label definitions are LABEL token,
-  ; while labels already defined are ACONST token with the associated address.
+  ;; new label definitions are LABEL token,
+  ;; while labels already defined are ACONST token with the associated address.
   (define (label-token lst)
     (let ((str (list->string lst)))
       (if (hash-table-exists? labels str)
@@ -114,7 +114,7 @@
         (else
           (list 'bad-character c))))))
     
-; ------ utility routines ------
+;;; ------ utility routines ------
 
 (define (hex-nibble v)
   (let ((masked (bitwise-and v #xf)))
@@ -159,10 +159,12 @@
   (or (eof-object? c)
       (char-whitespace? c)))
 
-; ------ these routines are called from the grammar ------
+;;; ------ routines called from the grammar ------
 
-; assembly base address
+;; assembly base address
 (define addr 0)
+
+;; symbol table mapping labels to associated addresses
 (define labels (make-hash-table))
 
 (define (list-line lst)
@@ -172,17 +174,17 @@
               lst)
     (display #\newline)))
 
-; assemble an instruction with 'implicit' addressing mode (no operands)
+;; assemble an instruction with 'implicit' addressing mode (no operands)
 (define (assemble-impl op)
   (assemble-op op 0)
   (list op))
 
-; assemble a register instruction; operand is register number
+;; assemble a register instruction; operand is register number
 (define (assemble-reg op reg-number)
   (assemble-op op reg-number)
   (list op (reg->char reg-number)))
 
-; assemble a memory instruction; operand is a 16 bit address
+;; assemble a memory instruction; operand is a 16 bit address
 (define (assemble-addr op addr)
   (assemble-op op 0)
   (assemble-word addr)
@@ -192,7 +194,7 @@
   (apply print args)
   (exit 1))
 
-; ------ start the parser ------
+;;; ------ main program: run the parser ------
 
 (titan-parser (make-lexer errorp) errorp)
 
